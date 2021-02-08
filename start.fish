f7700766762bff886aae732b2b536d9444282de7
Nathan Hare <me@laspruca.nz>
2021-02-08 17:38:06 +1300

    Update build script

--- start.fish
+++ start.fish
@@ -1,11 +1,13 @@
 #!/bin/fish
 cd (dirname (status -f))
 echo "Building Server"
-cargo install wasm-pack diesel_cli
+cargo install wasm-pack
+cargo install diesel_cli --no-default-features --features postgres
 cargo build --bin gib-server --release
 cd gib-server
 diesel setup
 diesel migration run
+mkdir static
 cd ..
 cd gib-web
 yarn install
